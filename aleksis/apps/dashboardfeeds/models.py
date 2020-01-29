239c6b21077374061d2001f4851f194d6bd037f3
Dominik George <nik@naturalnet.de>
2020-01-29 17:16:12 +0100

    Fix up some details and refactor base out to core

--- aleksis/apps/dashboardfeeds/models.py
+++ aleksis/apps/dashboardfeeds/models.py
@@ -1,64 +1,20 @@
 from typing import Optional
 
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
-from django.db import models
-from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
+
 import feedparser
-from .html_helper import parse_rss_html
 
-from constance import config
+from aleksis.core.models import DashboardWidget
 
-from aleksis.core.util import network
-
-
-class DashboardWidget(models.Model):
-    title = models.CharField(max_length=150, verbose_name=_("Widget Title"))
-    active = models.BooleanField(blank=True, verbose_name=_("Activate Widget"))
-    base_url = models.URLField(verbose_name=_("Base URL"),
-                               help_text=_("index url of the news website (as link for users)"))
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = _("Dashboard Widget")
-        verbose_name_plural = _("Dashboard Widgets")
+from .util.html_helper import parse_rss_html
 
 
-# class WordpressFeedWidget(models.Model):
-#     widget = models.OneToOneField(DashboardWidget, related_name="widget", verbose_name=_("widget"),
-#                                   on_delete=models.CASCADE)
-#     url = models.URLField(verbose_name=_("Wordpress Url"))
-# filter_vs_composer = models.BooleanField(verbose_name=_("Filter out Visual Composer Tags?"), blank=True, default=True)
-#
-#     # author_whitelist: list = None,
-#     # author_blacklist: list = None,
-#     # category_whitelist: list = None,
-#     # category_blacklist: list = None,
-#
-#     # https://wordpress.org/support/article/wordpress-feeds/#finding-your-feed-url
-#
-#     def get_context(self):
-#         feed = {
-#             "title": self.widget.title,
-#             "active": self.widget.active,
-#             "url": self.url,
-#             "results": network.get_newest_articles(domain=self.url, ),
-#         }
-#
-#     class Meta:
-#         verbose_name = _("Wordpress Widget")
-#         verbose_name_plural = _("Wordpress Widgets")
-
-
-class RSSFeedWidget(models.Model):
+class RSSFeedWidget(DashboardWidget):
     template = "dashboardfeeds/rss.html"
 
-    widget = models.OneToOneField(DashboardWidget, related_name="widget", verbose_name=_("widget"),
-                                  on_delete=models.CASCADE)
     url = models.URLField(verbose_name=_("RSS Url"))
+    base_url = models.URLField(verbose_name=_("Base URL"),
+                               help_text=_("index url of the news website (as link for users)"))
 
     def get_context(self):
         result = feedparser.parse(self.url)["entries"][0]

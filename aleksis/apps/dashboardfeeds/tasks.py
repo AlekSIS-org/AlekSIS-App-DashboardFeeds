65a5a040a7ad077b9ae55904f3da097f84b9dc37
Jonathan Weth <git@jonathanweth.de>
2021-02-16 19:11:16 +0100

    Make Celery non-optional

--- aleksis/apps/dashboardfeeds/tasks.py
+++ aleksis/apps/dashboardfeeds/tasks.py
@@ -1,9 +1,9 @@
 from feeds.utils import update_feeds
 
-from aleksis.core.util.core_helpers import celery_optional
+from aleksis.core.celery import app
 
 
-@celery_optional
+@app.task
 def get_feeds():
     """Update RSS feeds through django-feeds."""
     return update_feeds(max_feeds=10)
